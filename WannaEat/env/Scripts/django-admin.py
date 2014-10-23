ad48c6de1037f272358852cffdd6ef1202fa5933
KiraariK <kiraarik@sibmail.com>
2014-10-23 19:01:24 +0700

    Modify gitignore, cleanup repository.

--- WannaEat/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\οκ\documents\visual studio 2012\Projects\WannaEat\WannaEat\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
