406ba4d02dba8767c47264d1a8bba8cfc4cba593
KiraariK <kiraarik@sibmail.com>
2014-11-07 01:28:02 +0600

    Добавил тестовую страничку в Establishments. Она использует базовый template и бутстрап.

--- establishments/views.py
+++ establishments/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpRequest
 from django.shortcuts import render
+from django.template import RequestContext
 
-# Create your views here.
+
+def cities(request):
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'establishments/cities.html',
+        context_instance=RequestContext(request,
+            {
+                'title': 'Home page'
+            })
+    )
--- wanna_eat/urls.py
+++ wanna_eat/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'wanna_eat.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', RedirectView.as_view(url=r'city/')),
+    url(r'^city/', include('establishments.urls', namespace="establishments")),
 
     url(r'^admin/', include(admin.site.urls)),
 )
