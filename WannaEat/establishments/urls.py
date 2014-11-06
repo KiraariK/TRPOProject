27cb7070e4a97f17b288379c365aa9512585c6b6
KiraariK <kiraarik@sibmail.com>
2014-11-07 00:09:29 +0600

    Удалил старый VS2012 проект.

--- WannaEat/establishments/urls.py
+++
@@ -1,6 +0,0 @@
-﻿from django.conf.urls import patterns, url
-from establishments.views import CityList
-
-urlpatterns = patterns('',
-    url(r'^$', CityList.as_view(), name='home'),
-)
--- WannaEat/resources/admin.py
+++
@@ -1,8 +0,0 @@
-﻿from django.contrib import admin
-from establishments.models import Establishment, City
-from dishes.models import EstablishmentDishes, Dish
-
-admin.site.register(Establishment)
-admin.site.register(City)
-admin.site.register(EstablishmentDishes)
-admin.site.register(Dish)
