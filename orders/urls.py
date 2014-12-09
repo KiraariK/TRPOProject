2d6f01a79213df698032347ab02680994a6a85a1
KiraariK <kiraarik@sibmail.com>
2014-12-09 19:15:25 +0600

    Устранение мелкой ошибки flake8.

--- orders/urls.py
+++ orders/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns(
 
     url(r'cart/(?P<cart_state>\d+)/$', views.view_cart, name='cart'),
 
-    #url for ajax
+    # url for ajax
     url(r'cart_increment_dish/$', views.increment_dish, name='dish_incrementation'),
 
     # url for ajax
