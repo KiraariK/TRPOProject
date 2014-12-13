8f6c3bf6c2cea44a9035a1957d7cbdeba64f62fd
Kirill-Gavrilov <kirill.a.gavrilov@gmail.com>
2014-12-14 01:31:40 +0600

    Добавил авторизацию юзера.

--- employees/urls.py
+++ employees/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import patterns, url
+from employees import views
 from employees.views import EmployeePage
 
 urlpatterns = patterns(
     '',
     url(r'(?P<employees_id>\d+)/$', EmployeePage.as_view(), name='employees'),
+    url(r'^accounts/login/$', views.login_view, name='login_employee'),
+    url(r'^accounts/ban/$', views.ban_view, name='ban_employee'),
+    url(r'^accounts/login/login/$', views.auth_view, name='after_login'),
 )
--- employees/views.py
+++ employees/views.py
@@ -1,5 +1,7 @@
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from employees.models import Employee
+from django.contrib import auth
 
 
 class EmployeePage(ListView):
@@ -11,3 +13,39 @@ class EmployeePage(ListView):
         employees_id = self.kwargs.get('employees_id')
         context['employees'] = Employee.objects.get(id=employees_id)
         return context
+
+
+def login_view(request):
+    # здесь проверить зареган или нет. или хз где
+    return render(
+        request,
+        'employees/authentication.html',
+        {
+            'title': 'вход'
+        }
+    )
+
+
+def auth_view(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = auth.authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            user123 = Employee.objects.filter(user=user)[0]
+            auth.login(request, user)
+            return redirect('employees', user123.id)
+        else:
+            return redirect('ban_employee')
+    else:
+        return redirect('login_employee')
+
+
+def ban_view(request):
+    return render(
+        request,
+        'employees/ban.html',
+        {
+            'title': 'Sorry'
+        }
+    )
