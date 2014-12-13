9a598eb6a01ac483a4b9f6743a5aa8481e5d0b13
sir-de-mark <mrr1@tpu.ru>
2014-12-13 20:43:22 +0600

    Создал template для employees. Метод Order_acc не работает

--- employees/views.py
+++ employees/views.py
@@ -1,5 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
 from django.views.generic import ListView
 from employees.models import Employee
+from orders.models import Order
 
 
 class EmployeePage(ListView):
@@ -10,4 +13,18 @@ class EmployeePage(ListView):
         context = super().get_context_data(**kwargs)
         employees_id = self.kwargs.get('employees_id')
         context['employees'] = Employee.objects.get(id=employees_id)
+        context['order_list'] = Order.objects.filter(contact_account_id=employees_id)
+
         return context
+
+
+def acc_state(request):
+     if request.is_ajax():
+       # order_id = request.GET.get('id')
+       # order = Order.objects.get(id=int(order_id))
+       # Employee.acc_order(order_id)
+        return HttpResponse('1')
+     else: return HttpResponse('0')
+
+
+
